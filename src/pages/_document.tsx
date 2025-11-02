5ee39295759612824477d5e9e4622a8926feb015
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2025-11-03 00:38:27 +0100

    chore: use our self-hosted plausible and stop using umami

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,16 +8,8 @@ class RootDocument extends Document {
       <Html lang={'en'}>
         <Head>
           <DocumentHeadContent />
-          <script
-            defer
-            data-domain="classic.pokepc.net"
-            src="https://plausible.services.eonpixel.app/js/script.pageview-props.tagged-events.js"
-          />
-          <script
-            defer
-            src="https://umami.services.eonpixel.app/script.js"
-            data-website-id="9e305fb4-67c0-4357-b0d3-f94cc351137e"
-          />
+          {/* Plausible Analytics (Self-hosted): */}
+          <script defer data-domain="classic.pokepc.net" src="https://ua.werkowl.com/js/script.js" />
         </Head>
         <body>
           <Main />
